11157c5137dd59a1ca89dcc535f9cd4df22c3bde
user <lara.al_khatib@ensta-bretagne.org>
2021-12-01 15:36:22 +0100

    Algorithms

---
+++ Algorithms.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def iterative_bfs(graph):
+    visited = []
+    queue = deque()
+    init=True
+    while len(queue)!=0 | init:
+        if init:
+            voisin=graph.initial()
+        else:
+            node = queue.popleft()
+            voisin=graph.next(node)
+        for n in voisin:
+            if n not in visited:
+                queue.append(n)
+                visited.append(n)
+        init=False
+    return visited
+
+def isAcceptingBFS(graph):
+    visited = []
+    queue = deque()
+    init=True
+    while len(queue)!=0 | init:
+        if init:
+            voisin=graph.initial()
+        else:
+            node = queue.popleft()
+            voisin=graph.next(node)
+        for n in voisin:
+            if n not in visited:
+                if graph.isAccepting(n):
+                    return False
+                queue.append(n)
+                visited.append(n)
+        init=False
+    return True
