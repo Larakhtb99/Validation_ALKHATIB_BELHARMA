f9ff1cb443fc30af7154244a8eda79ac1a3b86b9
unknown <lara.al-khatib@ensta-bretagne.org>
2022-01-13 16:44:12 -0800

    Add find_accepting_bfs and get_trace

--- Algorithms.py
+++ Algorithms.py
@@ -3,7 +3,7 @@ from collections import deque
 from Kernel import STRTR, IsAcceptingProxy
 
 
-def iterative_bfs(graph):
+def bfs(graph):
     visited = []
     queue = deque()
     init=True
@@ -20,13 +20,47 @@ def iterative_bfs(graph):
         init=False
     return visited
 
+def find_accepting_bfs(graph):
+    visited = []
+    queue = deque()
+    init=True
+    while len(queue)!= 0 | init:
+        if init:
+            voisin = graph.initial()
+        else:
+            node = queue.popleft()
+            voisin=graph.next(node)
+        for n in voisin:
+            if n not in visited:
+                if graph.isAccepting(n):
+                    return False,n
+                queue.append(n)
+                visited.append(n)
+        init=False
+    return True,n
+
+def get_trace(parents, result, initial):
+    status,target = result
+    if not status :
+        print("L'accepting state n'est pas trouvé ")
+        return None
+    print (initial,result)
+
+    current_Node = target
+    trace = [current_Node]
+    while current_Node not in initial:
+        current_Node = parents[current_Node]
+        trace.append(current_Node)
+
+    print("Trace : ", trace)
+
 def iterative_bfs(graph):
     visited = []
     queue = deque()
     init=True
-    while len(queue)!=0 | init:
+    while len(queue)!= 0 | init:
         if init:
-            voisin=graph.initial()
+            voisin = graph.initial()
         else:
             node = queue.popleft()
             voisin=graph.next(node)
@@ -38,7 +72,6 @@ def iterative_bfs(graph):
                 visited.append(n)
         init=False
     return True
-
 
 def predicate_model_checker(semantic, predicate):
     tr = STRTR(semantic)
